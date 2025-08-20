72f5be6893032d8f833344dfb77bd66b4aaf10a4
jorgegmch <226594601+jorgegmch@users.noreply.github.com>
2025-08-20 16:50:34 -0500

    IMC exercise

--- Practica.py
+++ Practica.py
@@ -1,1 +1,18 @@
-print("Hola mundo")
+import os
+
+os.system("clear")
+
+print("Bienvenido a la app para determinar tu IMC\n")
+
+peso=float(input("Ingrese su peso: "))
+altura=float(input("Ingrese su altura: "))
+imc=peso/(altura**2)
+
+if imc<18.5:
+    print(f"Usted se encuentra por debajo de su peso ideal y su IMC es: {imc:.2f}")
+elif imc>=18.5 and imc<24.9:
+    print(f"Su peso es normal y su IMC es: {imc:.2f}")
+elif imc>=24.9 and imc<29.9:
+    print(f"Usted tiene sobrepeso y su IMC es: {imc:.2f}")
+else:
+    print(f"Usted tiene obesidad y su IMC es: {imc:.2f}")
